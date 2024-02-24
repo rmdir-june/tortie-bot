33cb36f2afed6240c9ffbd27e91be565d97b25f7
rmdir-june <thecenterofthree@gmaill.com>
2024-02-23 23:22:43 -0500

    a bunch of shit

--- main.py
+++ main.py
@@ -1,54 +1,37 @@
-import requests
-from bs4 import BeautifulSoup
 import discord
 from discord.ext import commands
-import time
-import os
-
-
-def tortie_requests():
-    try:
-        tortiesjson = requests.get("https://www.reddit.com/r/torties/top.json").json()
-        toppost_imagelink = tortiesjson['data']['children'][0]['data']['url_overridden_by_dest']
-        toppost_title = tortiesjson['data']['children'][0]['data']['title']
-        return toppost_title, toppost_imagelink
-    except KeyError:
-        time.sleep(1)
-        return tortie_requests()
-
-
-def tortie_parse(toppost_title, toppost_imagelink):
-    print(toppost_imagelink)
-    alt = f'r/torties - {toppost_title}'
-    r = requests.get(toppost_imagelink)
-    soup = BeautifulSoup(r.content)
-    img = soup.find(alt=alt)
-    print(img['src'])
-
-    with open("test.jpg", "wb") as f:
-        f.write(requests.get(img['src']).content)
-
+from datetime import datetime
+import json
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-
-while True:
-    title, imagelink = tortie_requests()
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
-    @client.event
-    async def on_ready():
-        print(f'We have logged in as {client.user}')
-        tortie_requests()
-        tortie_parse(title, imagelink)
-        channel = client.get_channel(1163200431103684639)
-        with open('test.jpg', 'rb') as f:
-            picture = discord.File(f)
-            await channel.send(file=picture)
+def manage_albumfile():
+    current_month = datetime.now().month
+    with open("albums.json", "r") as f:
+        data = json.load(f)
+        current_album = data[current_month]
+
+    return current_album
 
 
-    print(os.environ.get('TORTIE_TOKEN'))
-    client.run(os.environ.get('TORTIE_TOKEN'))
-    time.sleep(20)
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.command()
+async def add(ctx, *, newalbum):
+    current_month = datetime.now().month
+    await ctx.send(f'{newalbum} has been added to the queue')
+    with open("albums.json", "rw") as albumfile:
+        data = json.load(albumfile)
+        data(current_month = newalbum)
+
+
+@bot.command()
+async def current(ctx):
+    await ctx.send(f'The current album is {manage_albumfile()}')
